53ab1f513da6c782121985b4a5d48f09bc16dbfd
Michael Klapper <development@morphodo.com>
2013-01-01 13:38:42 +0100

    Rename use rst format for readme file

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Morphodo"
 maintainer_email "development@morphodo.com"
 license          "MIT"
 description      "Recipe to install PHP package manager"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rst'))
 version          "0.0.1"
 
 %w{ ubuntu debian centos redhat fedora }.each do |os|
