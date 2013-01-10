bc02a574def96784cc37c28ddaaf7cfd58a449be
Michael Klapper <development@morphodo.com>
2013-01-10 18:06:17 +0100

    Update attributes

--- resources/default.rb
+++ resources/default.rb
@@ -3,8 +3,8 @@ actions :install, :remove, :update
 default_action :install
 
 attribute :target_dir, :kind_of => String, :name_attribute => true
-attribute :global_bin_dir, :default => "/usr/local/bin", :kind_of => String
-attribute :global
-attribute :owner, :default => "root", :kind_of => [String, Integer]
-attribute :group, :default => "admin", :kind_of => [String, Integer]
-attribute :source, :default => "https://getcomposer.org/composer.phar", :kind_of => [String]
+attribute :global_bin_dir, :kind_of => String, :default => "/usr/local/bin"
+attribute :global, :equal_to => [true, false], :default => true
+attribute :owner, :kind_of => [String, Integer], :default => "root"
+attribute :group, :kind_of => [String, Integer], :default => "admin"
+attribute :source, :kind_of => [String], :default => "https://getcomposer.org/composer.phar"
