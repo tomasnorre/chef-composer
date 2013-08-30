6d5242926778288f38327802491233179edcafc8
Michael Klapper <development@morphodo.com>
2013-08-30 16:21:44 +0200

    Update syntax error

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,4 +4,4 @@ default["composer"]["group"] = "www-data"
 default["composer"]["target_dir"] = "/opt/composer"
 default["composer"]["global_bin_dir"] = "/usr/local/bin"
 default["composer"]["global"] = true
-default["composer"]["source"] = "https"//getcomposer.org/composer.phar"
+default["composer"]["source"] = "https://getcomposer.org/composer.phar"
