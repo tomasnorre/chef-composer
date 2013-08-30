27cb886029fc28e08ca80ab0b15408b412bb09b7
Michael Klapper <development@morphodo.com>
2013-08-30 11:36:31 +0200

    Increase version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "development@morphodo.com"
 license          "MIT"
 description      "Recipe to install PHP package manager"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rst'))
-version          "0.2.0"
+version          "0.2.1"
 
 %w{ ubuntu debian centos redhat fedora }.each do |os|
 	supports os
