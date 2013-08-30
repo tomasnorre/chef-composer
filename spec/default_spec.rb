00c5805d25899985f10076565dab7bfc46ef1b44
Michael Klapper <development@morphodo.com>
2013-08-30 13:46:58 +0200

    Update

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,8 +1,23 @@
-require 'chefspec'
+require 'spec_helper'
 
-describe 'my_new_cookbook::default' do
-  let(:chef_run) { ChefSpec::ChefRunner.new.converge 'composer::default' }
-  it 'install composer globally' do
-    expect(chef_run).to create_directory '/opt/composer'
-  end
+describe 'composer::default' do
+  let(:version)  { '1.0.0' }
+  let(:owner)  { 'root' }
+  let(:group)  { 'www-data' }
+
+  let(:runner) {
+    runner = ChefSpec::ChefRunner.new(platform: 'ubuntu', version: '12.04')
+
+    runner.converge('composer::default')
+  }
+
+  #it 'install composer globally' do
+  #  expect(runner).to create_directory '/opt/composer'
+  #end
+
+  #it 'creates the symlink' do
+  #  link = runner.link('composer')
+  #  expect(link.target_file).to eq('/usr/local/bin/phantomjs')
+  #  expect(link.to).to eq("/usr/local/#{basename}/bin/phantomjs")
+  #end
 end
